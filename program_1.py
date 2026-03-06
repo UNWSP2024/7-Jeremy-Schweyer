0e0bd6ccf46aed9d8a99667f6663bfd97c4d8b3f
Jeremy-Schweyer <jtschweyer@students.unwsp.edu>
2026-03-06 16:09:08 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,4 +1,12 @@
-# Program #1: Rainfall
-# Design a program that lets the user enter the total rainfall for each of 12 months into a list.
-# The program should calculate and display the total rainfall for the year, 
-# the average monthly rainfall, # and the months with the highest and lowest amounts.
+rainfall = []
+
+for i in range(1,13):
+    rainfal = int(input("Enter the rainfall for every month: "))
+    rainfall.append(rainfal)
+
+total = sum(rainfall)
+average = total/12
+print("The average rainfall is: ", average)
+print("The total rainfall is: ", total)
+print("The Maximum rainfall is: ", max(rainfall))
+print("The Minimum rainfall is: ", min(rainfall))
